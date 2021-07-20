baea293a7f652b8bb34aba4b7835c44aa93546cb
amh092 <ahmedalhazza@Ahmeds-MacBook-Pro.local>
2021-07-20 06:02:30 +0300

    js beutify

--- main.js
+++ main.js
@@ -8,28 +8,23 @@ var cantBeZero = document.querySelector(".number-text h1:last-of-type");
 var tipCalc;
 var totalCalc;
 
-for(i = 0 ; i < btns.length; i = i +1){
+for (i = 0; i < btns.length; i = i + 1) {
     btns2.push(btns[i])
 }
 
-for(i = 0; i < btns2.length; i = i +1 ){
-   
-    btns2[i].addEventListener("click",function(e){
-        if ( numberOfPeople.value == 0 ){
-            cantBeZero.classList.remove("hidden")}
+for (i = 0; i < btns2.length; i = i + 1) {
 
-       else if  ( numberOfPeople.value > 0 && billAmount.value > 0 ) {
+    btns2[i].addEventListener("click", function (e) {
+            if (numberOfPeople.value == 0) {
+                cantBeZero.classList.remove("hidden")
+            } else if (numberOfPeople.value > 0 && billAmount.value > 0) {
                 cantBeZero.classList.add("hidden")
-                tipCalc =   parseInt(billAmount.value) * (parseInt(e.target.innerText) /100 ) / parseInt(numberOfPeople.value) ;
+                tipCalc = parseInt(billAmount.value) * (parseInt(e.target.innerText) / 100) / parseInt(numberOfPeople.value);
                 tipAmount.innerText = Math.ceil(tipCalc)
-                totalCalc  = (parseInt(billAmount.value) / parseInt(numberOfPeople.value)) + tipCalc;
+                totalCalc = (parseInt(billAmount.value) / parseInt(numberOfPeople.value)) + tipCalc;
                 totalForPerson.innerText = Math.round(totalCalc);
-           
-            }
-
-   
-    
-    })
-}
 
 
+
+            })
+    }
