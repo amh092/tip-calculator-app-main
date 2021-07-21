96b1a8d22b14c960966c2c18e0e9f8f23138041a
amh092 <ahmedalhazza@Ahmeds-MacBook-Pro.local>
2021-07-21 15:42:39 +0300

    staged

--- main.js
+++ main.js
@@ -8,29 +8,78 @@ var cantBeZero = document.querySelector(".number-text h1:last-of-type");
 var tipCalc;
 var totalCalc;
 var custom = document.getElementById("custom");
+var reset = document.getElementById("reset");
+var tipPercent ;
 
 for (i = 0; i < btns.length; i = i + 1) {
     btns2.push(btns[i])
 }
 
+function calc(tipPercent) {
+    
+    if (numberOfPeople.value == 0) {
+        cantBeZero.classList.remove("hidden")
+    } else if (numberOfPeople.value > 0 && billAmount.value > 0) {
+        cantBeZero.classList.add("hidden")
+        tipCalc = parseInt(billAmount.value) * (parseInt(tipPercent) / 100) / parseInt(numberOfPeople.value);
+        tipAmount.innerText = tipCalc.toFixed(2)
+        totalCalc = (parseInt(billAmount.value) / parseInt(numberOfPeople.value)) + tipCalc;
+        totalForPerson.innerText = totalCalc.toFixed(2)
+    }
+    if (totalCalc == NaN) {
+
+        totalCalc.innerText = "ERROR"
+        
+    }
+}
+
+custom.addEventListener("input", function () {
+    calc(custom.value)
+    tipPercent = custom.value
+    
+})
+
 for (i = 0; i < btns2.length; i = i + 1) {
 
     btns2[i].addEventListener("click", function (e) {
-            if (numberOfPeople.value == 0) {
-                cantBeZero.classList.remove("hidden")
-            } else if (numberOfPeople.value > 0 && billAmount.value > 0) {
-                cantBeZero.classList.add("hidden")
-                tipCalc = parseInt(billAmount.value) * (parseInt(e.target.innerText) / 100) / parseInt(numberOfPeople.value);
-                tipAmount.innerText = Math.ceil(tipCalc)
-                totalCalc = (parseInt(billAmount.value) / parseInt(numberOfPeople.value)) + tipCalc;
-                totalForPerson.innerText = Math.round(totalCalc);
-
-            }
-            })
+        calc(e.target.innerText)
+        tipPercent = e.target.innerText
     }
+    )
+   
+}
 
-custom.addEventListener("blur", function(){
-
-    
-
+billAmount.addEventListener("input",function (){
+    if (numberOfPeople.value == 0) {
+        cantBeZero.classList.remove("hidden")
+    } else if (numberOfPeople.value > 0 && billAmount.value > 0) {
+        cantBeZero.classList.add("hidden")
+        tipCalc = parseInt(billAmount.value) * (parseInt(tipPercent) / 100) / parseInt(numberOfPeople.value);
+        tipAmount.innerText = tipCalc.toFixed(2)
+        totalCalc = (parseInt(billAmount.value) / parseInt(numberOfPeople.value)) + tipCalc;
+        totalForPerson.innerText = totalCalc.toFixed(2)
+    }   
 })
+
+numberOfPeople.addEventListener("input",function(){
+    if (numberOfPeople.value == 0) {
+        cantBeZero.classList.remove("hidden")
+    } else if (numberOfPeople.value > 0 && billAmount.value > 0) {
+        cantBeZero.classList.add("hidden")
+        tipCalc = parseInt(billAmount.value) * (parseInt(tipPercent) / 100) / parseInt(numberOfPeople.value);
+        tipAmount.innerText = tipCalc.toFixed(2)
+        totalCalc = (parseInt(billAmount.value) / parseInt(numberOfPeople.value)) + tipCalc;
+        totalForPerson.innerText =  totalCalc.toFixed(2);
+    }}
+)
+
+
+
+reset.addEventListener("click", function () {
+    billAmount.value = "";
+    numberOfPeople.value = "";
+    custom.value = "";
+    totalForPerson.innerText = "$0:00";
+    tipAmount.innerText = "$0:00"
+})
+
